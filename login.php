ab399ca2cfc9f0502b29c1b4397b52bacba0f0b2
MFJLBeuzel <maartenbeuzel@gmail.com>
2016-05-27 17:24:04 +0200

    Commit
    commit

--- login.php
+++ login.php
@@ -1,38 +1,27 @@
-<?php include 'header.php'; ?>
+<?php
+	session_start();
+	include 'connect.php';
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	if(empty($_POST['username']) || empty($_POST['password'])) {
+		header('location:loginscreen.php?fout');
+	} else {
+		$sql = "SELECT * FROM dbo.GEBRUIKER WHERE GEBRUIKERSNAAM = '$username' AND
+    WACHTWOORD = '$password'";
+		$result = sqlsrv_query($conn, $sql);
+		if($result === false){
+			die( print_r( sqlsrv_errors(), true));
+		}
+		if(!sqlsrv_fetch($result)){
+			header('location:loginscreen.php?fout2');
+		} else {
+			while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+				$username = $row['gebruikersnaam'];
+			}
+			echo 'logged in?';
+			$_SESSION['username'] = $username;
+			header('location:index.php');
+		}
 
-<html>
-  <div class="row">
-    <div class="content">
-      <div class="large-8 columns">
-        <form action="inloggen.php" method="post">
-          <table>
-            <tr>
-              <td>
-                Username:
-              </td>
-              <td>
-                <input type="text" name="username" placeholder="Username">
-              </td>
-              </tr>
-              <tr>
-              <td>
-                Password:
-              </td>
-              <td>
-                <input type="password" name="password" placeholder="Password">
-              </td>
-              </tr>
-                </table>
-                  <input type="submit" value="Login" class="rechts smallbtn">
-              <br/>
-              <br/>
-                <a href="register.php" class="clicklink">Register</a>
-              </form>
-              <br/>
-              <br/>
-      </div>
-    </div>
-  </div>
-</body>
-
-</html>
+	}
+?>
