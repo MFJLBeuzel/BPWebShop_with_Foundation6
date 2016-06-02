516b315ef23403deb06b20f445d965f7afdd7c14
MFJLBeuzel <maartenbeuzel@gmail.com>
2016-06-02 14:15:21 +0200

    Intro page
    commit

--- about.php
+++ about.php
@@ -3,30 +3,31 @@
 ?>
 
  <div class="main about_page">
-   <h1>Over ons</h1>
+  <h1>About</h1>
    <div class="artikel">
-     <h2>Wie zijn wij</h2>
-     <p>
+    <h2>What do we offer?</h2>
+      <p>
        <img src="images/Maarten.jpg" width="200" height="250">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-       ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-       ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-       sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-       est laborum.
-     </p>
+     </p></br>
+      <p>
+        Onze martkplaats verbindt kopers en verkopers van overal ter wereld met
+        elkaar. Met meer dan 23 miljoen producten beschikbaar en duizenden
+        verkopers, is dit de beste plek om nieuwe uitgebrachtte albums tot
+        zeldzame verzamelobjecten te vinden. Omdat de marktplaats gebouwd is
+        op een zeer accurate database, is het gemakkelijk voor verkopers om hun
+        producten op de site aan te bieden. Tegelijkertijd is dit voor kopers
+        ook ideaal, aangezien zij sneller de producten kunnen vinden waar
+        zij specifiek naar zoeken.
+      </p>
    </div>
    <div class="artikel">
-     <h2>Routebeschrijving</h2>
+    <h2>How to find us</h2>
      <p>
-       <iframe src=
-       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2455.6930599261304!2d6.352781151697793!3d52.01247077962165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7858478c73dcb%3A0xf547d1f51aa93680!2sArdinckhoek+33%2C+7021+EX+Zelhem!5e0!3m2!1snl!2snl!4v1456060312777" width="400" height="275">
-       </iframe>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-       ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-       ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-       sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-       est laborum.
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2455.
+       9952662372075!2d6.297765351388833!3d52.00696418177502!2m3!1f0!2f0!3f0!3m2!
+       1i1024!2i768!4f13.1!3m3!1m2!1s0x47c78549525a70d5%3A0xd8488e6b7c950326!
+       2sEckhorsterstraat%2C+Zelhem!5e0!3m2!1snl!2snl!4v1464869227256"
+       width="400" height="300" frameborder="0" style="border:0"
+       allowfullscreen></iframe>
      </p>
    </div>
